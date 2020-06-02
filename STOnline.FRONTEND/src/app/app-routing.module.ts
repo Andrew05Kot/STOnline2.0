8a98ed18fb78278fb7300448e15916d157404696
Andrii Kotiuha <akotiuha@yukon.cv.ua>
2020-06-02 19:04:10 +0300

    Add home and not found pages to frontend

--- STOnline.FRONTEND/src/app/app-routing.module.ts
+++ STOnline.FRONTEND/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {NotFoundPageComponent} from "./not-found-page/not-found-page.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'not-found', component: NotFoundPageComponent},
+  {path: '**', redirectTo: 'not-found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- STOnline.FRONTEND/src/app/app.module.ts
+++ STOnline.FRONTEND/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
